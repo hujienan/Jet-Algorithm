948433bf0cbcf53b18a03a5cf98cee3d40d26c0e
Jet Hu <774372605@qq.com>
2021-09-19 19:13:19 +0800

    88. Merge Sorted Array

---
+++ leetcode/88. Merge Sorted Array/index.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def merge1(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        nums1[:n] = nums2[:n]
+
+    def merge2(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[:] = sorted(nums1[:m]+nums2[:])
+
+
+solution = Solution()
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+solution.merge1(nums1, m, nums2, n)
+assert nums1 == [1, 2, 2, 3, 5, 6], "Should be [1,2,2,3,5,6]"
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+solution.merge2(nums1, m, nums2, n)
+assert nums1 == [1, 2, 2, 3, 5, 6], "Should be [1,2,2,3,5,6]"
