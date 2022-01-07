32c9ca95138b1cef4dc485abd98ded3bbb6ce5d8
Jet Hu <774372605@qq.com>
2022-01-07 13:47:46 +0800

    graph way to solve finding the judge problem

--- leetcode/997. Find the Town Judge/index.py
+++ leetcode/997. Find the Town Judge/index.py
@@ -5,16 +5,27 @@ class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         trusts = DefaultDict(int)
         trustees = DefaultDict(int)
-        
+
         for item in trust:
             trusts[item[0]] += 1
             trustees[item[1]] += 1
-        
+
         for i in range(1, n+1):
             if trusts[i] == 0 and trustees[i] == n-1:
                 return i
         return -1
-            
+
+    def findJudge1(self, n: int, trust: List[List[int]]) -> int:
+        degree = [0] * (n + 1)
+        for x, y in trust:
+            degree[x] -= 1
+            degree[y] += 1
+        for i in range(1, n+1):
+            if degree[i] == n-1:
+                return i
+        return -1
+
+
 solution = Solution()
-assert solution.findJudge(2, [[1,2]]) == 2, "Should be 2"
-assert solution.findJudge(3, [[1,3],[2,3],[3,1]]) == -1, "Should be -1"
+assert solution.findJudge(2, [[1, 2]]) == 2, "Should be 2"
+assert solution.findJudge1(3, [[1, 3], [2, 3], [3, 1]]) == -1, "Should be -1"
