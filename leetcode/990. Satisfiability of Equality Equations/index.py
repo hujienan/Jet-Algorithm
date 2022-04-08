9b67e544b4ea902ca029c52feb646d3b1942359e
Jet Hu <774372605@qq.com>
2022-04-08 11:01:53 +0800

    connected components for 990 satisfiability of equality equations

--- leetcode/990. Satisfiability of Equality Equations/index.py
+++ leetcode/990. Satisfiability of Equality Equations/index.py
@@ -1,32 +1,58 @@
 from collections import defaultdict
-from operator import eq
 from typing import List
 
 
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
-      dic = defaultdict(list)
-      for e in equations:
-        if e[1] == '=':
-          dic[e[0]].append(e[3])
-          dic[e[3]].append(e[0])
-      
-      def check(c):
-        visited.add(c)
-        for n in dic[c]:
-          if n not in visited:
-            check(n)
+        dic = defaultdict(list)
+        for e in equations:
+            if e[1] == '=':
+                dic[e[0]].append(e[3])
+                dic[e[3]].append(e[0])
 
-      for e in equations:
-        if e[1] == '!':
-          visited = set()
-          check(e[0])
-          if e[3] in visited:
-            return False
-      
-      return True
+        def check(start):
+            visited.add(start)
+            for c in dic[start]:
+                if c not in visited:
+                    check(c)
+
+        for e in equations:
+            if e[1] == '!':
+                visited = set()
+                check(e[0])
+                if e[3] in visited:
+                    return False
+
+        return True
+
+    def equationsPossible1(self, equations: List[str]) -> bool:
+        dic = defaultdict(list)
+        for e in equations:
+            if e[1] == '=':
+                dic[e[0]].append(e[3])
+                dic[e[3]].append(e[0])
+
+        colors = defaultdict(list)
+        for k in dic:
+            stack = [k]
+            while stack:
+                c = stack.pop()
+                for n in dic[c]:
+                    if n not in colors[k]:
+                        colors[k].append(n)
+                        stack.append(n)
+
+        for e in equations:
+            if e[1] == '!':
+                if e[0] == e[3]:
+                    return False
+                if e[3] in colors[e[0]]:
+                    return False
+        return True
+
+
 
 solution = Solution()
-
 equations = ['a==b', 'b!=c', 'a==c']
-assert solution.equationsPossible(equations) == False, "Should be false"
+
+assert solution.equationsPossible1(equations) == False, "Should be false"
