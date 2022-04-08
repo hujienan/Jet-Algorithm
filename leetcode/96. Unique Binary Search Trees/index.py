7dabfde6b6f670218d295445922bbdd336cc89bb
Jet Hu <774372605@qq.com>
2022-04-08 11:59:04 +0800

    updated code for unique bst

--- leetcode/96. Unique Binary Search Trees/index.py
+++ leetcode/96. Unique Binary Search Trees/index.py
@@ -1,11 +1,12 @@
 class Solution:
     def numTrees(self, n: int) -> int:
         dp = [1] * (n+1)
-        for nodes in range(2, n+1):
+        dp[1] = 1
+        for i in range(2, n+1):
             total = 0
-            for i in range(1, nodes+1):
-                total += dp[i-1] * dp[nodes-i]
-            dp[nodes] = total
+            for index in range(i):
+                total += dp[index] * dp[i - index - 1]
+            dp[i] = total
         return dp[n]
 
 
