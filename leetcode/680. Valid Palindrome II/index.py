ef0576e0271444c8d22cca14752c8c01f05d691b
Jet Hu <774372605@qq.com>
2022-05-11 09:49:57 +0800

    680. Valid Palindrome II

---
+++ leetcode/680. Valid Palindrome II/index.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                one = s[left:right]
+                two = s[left+1:right+1]
+                return one == one[::-1] or two == two[::-1]
+            left += 1
+            right -= 1
+        return True
+
+solution = Solution()
+s = "abca"
+assert solution.validPalindrome(s) == True, "Should be True"
