f7c824f394917b0ce426e2bc378d4962378bb136
Jet Hu <774372605@qq.com>
2022-05-27 10:03:23 +0800

    1342. Number of Steps to Reduce a Number to Zero

---
+++ leetcode/1342. Number of Steps to Reduce a Number to Zero/index.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        res = 0
+        while num:
+            if num % 2 == 0:
+                num = num // 2
+            else:
+                num -= 1
+            res += 1
+        return res
+
+solution = Solution()
+assert solution.numberOfSteps(123) == 12, "Should be 12"
