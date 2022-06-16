bb2ecc430c3ca44f7d07350c6f1ab1032c0dda3d
Jet Hu <774372605@qq.com>
2022-06-16 16:27:48 +0800

    BFS for 111. Minimum Depth of Binary Tree

--- leetcode/111. Minimum Depth of Binary Tree/index.py
+++ leetcode/111. Minimum Depth of Binary Tree/index.py
@@ -4,11 +4,13 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 from typing import Optional
 
 
 class Solution:
-    def minDepth(self, root: Optional[TreeNode]) -> int:
+    def minDepth1(self, root: Optional[TreeNode]) -> int:
+        # DFS
         def t(node):
             if not node:
                 return 0
@@ -22,3 +24,22 @@ class Solution:
                 cur = r
             return 1 + cur
         return t(root)
+    
+    def minDepth2(self, root: Optional[TreeNode]) -> int:
+        # BFS
+        if not root:
+            return 0
+        q = deque([root])
+        depth = 1
+        while q:
+            length = len(q)
+            for _ in range(length):
+                n = q.popleft()
+                if not n.left and not n.right:
+                    return depth
+                if n.left:
+                    q.append(n.left)
+                if n.right:
+                    q.append(n.right)
+            depth += 1
+        return depth
