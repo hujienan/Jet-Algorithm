deb6a14175a4d27d98b731f983bcc481120c58b1
Jet Hu <774372605@qq.com>
2022-10-09 10:46:59 +0800

    653. Two Sum IV - Input is a BST

---
+++ leetcode/653. Two Sum IV - Input is a BST/index.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        temp = set()
+        def t(node):
+            if not node:
+                return False
+            if node.val in temp:
+                return True
+            target = k - node.val
+            temp.add(target)
+            return t(node.left) or t(node.right)
+        return t(root)
