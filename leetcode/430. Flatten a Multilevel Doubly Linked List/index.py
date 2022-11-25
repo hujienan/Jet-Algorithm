45d14faedd3c908c00afbc1cfea383a292eecb9f
Jet Hu <774372605@qq.com>
2022-11-25 14:36:25 +0800

    430. Flatten a Multilevel Doubly Linked List

---
+++ leetcode/430. Flatten a Multilevel Doubly Linked List/index.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        cur = head
+        stack = []
+        while cur:
+            if cur.child:
+                if cur.next:
+                    stack.append(cur.next)
+                cur.next = cur.child
+                cur.next.prev = cur
+                cur.child = None
+            elif not cur.next and stack:
+                cur.next = stack.pop()
+                cur.next.prev = cur
+            cur = cur.next
+        return head
