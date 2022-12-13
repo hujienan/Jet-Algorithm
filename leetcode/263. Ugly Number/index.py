a8928f4d71c8d73db99ef7343dcecbdadd39a0d1
Jet Hu <774372605@qq.com>
2022-12-13 10:17:06 +0800

    updated the solution for Ugly Number

--- leetcode/263. Ugly Number/index.py
+++ leetcode/263. Ugly Number/index.py
@@ -2,9 +2,12 @@ class Solution:
     def isUgly(self, n: int) -> bool:
         if n < 1:
             return False
-        for i in [2,3,5]:
-            while n % i == 0:
-                n = n // i
+        while n % 2 == 0:
+            n /= 2
+        while n % 3 == 0:
+            n /= 3
+        while n % 5 == 0:
+            n /= 5
         return n == 1
 
 solution = Solution()
